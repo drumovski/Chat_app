42b2632db686ade66e6993d07eb8ed5f129e8696
Jason Stacy <jason.stacy@gmail.com>
2022-08-06 13:52:52 +1000

    add chat functionality

--- server.js
+++ server.js
@@ -2,10 +2,27 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.broadcast.emit('chat message','a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        socket.broadcast.emit('chat message','a user disconnected');
+      });
+
+      socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+          socket.broadcast.emit('chat message', msg);
+        });   
+})
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
